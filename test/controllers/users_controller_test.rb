50f05d6fe59905246970c0249a9fc24a2580cb98
Baptiste7512 <80527980+Baptiste7512@users.noreply.github.com>
2022-04-20 10:31:26 +0200

    Create User

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+
+  test 'should get new' do
     get users_new_url
     assert_response :success
+  end
+
+  def setup
+    @user = User.new(name: 'Example User', email: 'user@example.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = ''
+    assert_not @user.valid?
+  end
+  test 'email should be present' do
+    @user.email = ' '
+    assert_not @user.valid?
   end
 end
